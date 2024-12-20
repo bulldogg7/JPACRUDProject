be9fd60b347290aa130b81b6775a814a31c57bb6
bulldogg7 <163910022+bulldogg7@users.noreply.github.com>
2024-12-20 15:03:00 -0700

    .

--- JPACookie/src/main/java/com/skilldistillery/jpacookie/entities/Cookie.java
+++ JPACookie/src/main/java/com/skilldistillery/jpacookie/entities/Cookie.java
@@ -1,5 +1,24 @@
 package com.skilldistillery.jpacookie.entities;
 
+@Entity
 public class Cookie {
 
+		@Id
+		@GeneratedValue(strategy = GenerationType.IDENTITY)
+		private int id;
+
+		private String type;
+
+		private String frosting;
+
+		private String topping;
+
+		private Integer calories;
+		
+		@Column(name="sugar_grams")
+		private Integer sugar;
+
+
+		
+	
 }
