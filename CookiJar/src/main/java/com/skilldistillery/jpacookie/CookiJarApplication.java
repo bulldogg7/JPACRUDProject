265ffb0e6cfabc6347c9da4373c281cd40e28173
bulldogg7 <163910022+bulldogg7@users.noreply.github.com>
2024-12-20 16:01:10 -0700

    Added Spring Boot

---
+++ CookiJar/src/main/java/com/skilldistillery/jpacookie/CookiJarApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpacookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookiJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookiJarApplication.class, args);
+	}
+
+}
