b895254e5668dfa452240315e5bcefcec43f5e95
bulldogg7 <163910022+bulldogg7@users.noreply.github.com>
2024-12-20 15:22:34 -0700

    .

--- JPACookie/src/main/java/com/skilldistillery/jpacookie/entities/Cookie.java
+++ JPACookie/src/main/java/com/skilldistillery/jpacookie/entities/Cookie.java
@@ -1,5 +1,11 @@
 package com.skilldistillery.jpacookie.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 @Entity
 public class Cookie {
 
--- JPACookie/src/test/java/com/skilldistillery/jpacookie/entities/CookieTest.java
+++ JPACookie/src/test/java/com/skilldistillery/jpacookie/entities/CookieTest.java
@@ -1,12 +1,17 @@
 package com.skilldistillery.jpacookie.entities;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 class CookieTest {
 
